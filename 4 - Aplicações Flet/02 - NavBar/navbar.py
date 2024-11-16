83fb5d1bf6c0cd0ab2d90db3d21c22631602768e
rdilimas <rdilimas@gmail.com>
2024-11-16 00:41:14 -0300

    feat: adicionando informações de norificações

--- 4 - Aplicações Flet/02 - NavBar/navbar.py
+++ 4 - Aplicações Flet/02 - NavBar/navbar.py
@@ -2,13 +2,17 @@ from flet import *
 
 def main(page:Page):
 
+    chamadasPerdidas = "5"
+
     page.title = "Usando NavBar"  
 
     page.navigation_bar = NavigationBar(
         destinations=[
             NavigationBarDestination(icon=icons.HOME, label="Home"),
             NavigationBarDestination(icon=icons.CONTACTS, label="Contatos"),
-            NavigationBarDestination(icon=icons.CHAT, label="Mensagem")
+            NavigationBarDestination(icon=icons.CHAT, label="Mensagem"),
+            #icon_content para adicionar informação de notificações na aplicação, ex: chamadas perdidas e mensagen...
+            NavigationBarDestination(icon_content=Badge(content=Icon(icons.PHONE), text=chamadasPerdidas), label="Telefone")
         ]
     )
 
